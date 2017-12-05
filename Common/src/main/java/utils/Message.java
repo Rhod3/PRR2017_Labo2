8cbe8c3fcaae8e35e3991744a0af23b82a1efc35
Basile portable <basile.chatillon@heig-vd.ch>
2017-12-05 16:24:54 +0100

    update de lamport pour que ça fasse les bons envois via RMI

--- Common/src/main/java/utils/Message.java
+++ Common/src/main/java/utils/Message.java
@@ -2,5 +2,41 @@ package utils;
 
 import java.io.Serializable;
 
-public class Message implements Serializable{
+public class Message implements Serializable {
+    private MessageType messageType;
+    private int timeStamp;
+    private int sender;
+
+    public Message() {
+    }
+
+    public Message(MessageType messageType, int timeStamp, int sender) {
+        this.messageType = messageType;
+        this.timeStamp = timeStamp;
+        this.sender = sender;
+    }
+
+    public MessageType getMessageType() {
+        return messageType;
+    }
+
+    public void setMessageType(MessageType messageType) {
+        this.messageType = messageType;
+    }
+
+    public int getTimeStamp() {
+        return timeStamp;
+    }
+
+    public void setTimeStamp(int timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+
+    public int getSender() {
+        return sender;
+    }
+
+    public void setSender(int sender) {
+        this.sender = sender;
+    }
 }
--- Handler/src/main/java/Lamport.java
+++ Handler/src/main/java/Lamport.java
@@ -1,17 +1,23 @@
+import remoteInterfaces.ILamport;
+import utils.Message;
+import utils.MessageFree;
 import utils.MessageType;
 
+import java.rmi.Naming;
+
 public class Lamport {
-    private static int id = 0;
     private MessageType[] fileMessage;
     private int[] fileTimeStamp;
     private int numberSite;
     private int me;
     private int logicalClock;
     private boolean csGranted;
+    private int sharedValue;
 
-    public Lamport(int numberSite) {
+    public Lamport(int numberSite, int me) {
+        this.sharedValue = 0;
         this.numberSite = numberSite;
-        this.me = id++;
+        this.me = me;
         this.logicalClock = 0;
         this.csGranted = false;
         fileMessage = new MessageType[numberSite];
@@ -59,8 +65,13 @@ public class Lamport {
         csGranted = false;
     }
 
-    public void receive(MessageType messageType, int timeStamp, int sender) {
+    public void receive(Message message) {
+        MessageType messageType = message.getMessageType();
+        int timeStamp = message.getTimeStamp();
+        int sender = message.getSender();
+
         logicalClock = Math.max(logicalClock, timeStamp) + 1;
+
         switch (messageType) {
             case REQUEST:
                 fileMessage[sender] = MessageType.REQUEST;
@@ -80,15 +91,32 @@ public class Lamport {
                 fileMessage[sender] = MessageType.FREE;
                 fileTimeStamp[sender] = timeStamp;
 
+                sharedValue = ((MessageFree) message).getNewSharedValue();
                 break;
         }
 
         csGranted = fileMessage[me] == MessageType.REQUEST && permission(me);
     }
 
-    public void send(MessageType message, int destination) {
-        // ENVOIE((message, logicalClock, me), destination);
+    public void send(MessageType messageType, int destination) {
+        try {
+            // Getting the registry
+            // Registry registry = LocateRegistry.getRegistry(10999);
+
+            // Looking up the registry for the remote object
+            ILamport lamport = (ILamport) Naming.lookup("Handler" + destination);
+
+            // Calling the remote method using the obtained object
+            if (messageType == MessageType.FREE)
+                lamport.receive(new MessageFree(messageType, logicalClock, me, sharedValue));
+            else
+                lamport.receive(new Message(messageType, logicalClock, me));
+
+            // System.out.println("Remote method invoked");
+        } catch (Exception e) {
+            System.err.println("Handler exception: " + e.toString());
+            e.printStackTrace();
+
+        }
     }
-
-
 }
