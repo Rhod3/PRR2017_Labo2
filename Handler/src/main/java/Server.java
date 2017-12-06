0e9ca2d7775d5dfd3dfb569c3ce0676d93779d95
Nicolas Rod <ncls.rod@gmail.com>
2017-12-06 22:46:17 +0100

    Finished doc

--- Handler/src/main/java/Server.java
+++ Handler/src/main/java/Server.java
@@ -1,3 +1,59 @@
+/**
+ * PRR Laboratoire 2 - Basile Châtillon & Nicolas Rod
+ *
+ * === Structure ===
+ * Notre laboratoire est structuré en 3 modules:
+ * - un module App, qui représente une tâche applicative
+ * - un module Handler, qui représente le gestionnaire de variable globale
+ * - un module Common, qui contient tout les éléments commun au module App et Handler
+ * Ces 3 modules sont nécessaires pour exécuter un site du système réparti.
+ *
+ * Afin de mettre en place le système réparti à N sites, il faut procéder de la manière suivant:
+ * - Lancer les N Server avec comme paramètres [ID du site] [N]
+ * - Lancer les N App avec comme paramètre [ID du site]
+ * Sachant que les ID de site sont des entiers allant de 0 à N-1.
+ *
+ * === Commentaires ===
+ * Dans notre laboratoire, la gestion des URL des différents RMI registry est la suivante: chaque Server va mettre
+ * à disposition un object LamportHandler à l'adresse rmi://localhost:1099/LamportX , où X est l'ID du site
+ * que l'on souhaite joindre. Ainsi, notre système réparti est uniquement utilisable en local sur une seule machine en
+ * l'état: il faudrait changer la gestion des adresses RMI pour que l'on puisse indiquer à chaque site ou se
+ * situe les autres sites (par exemple à l'aide d'un fichier de configuration externe) que l'on donnerait à
+ * chaque site à leur démarrage.
+ *
+ * Nous avons décidé de placer la variable partagée dans la classe LamportHandler. Pour ce laboratoire, cette
+ * variable est de type int pour rester simple. Idéalement, le type aurait pu être générique.
+ * Les tâches applicatives peuvent donc accéder à cet variable et la modifier en proposant une nouvelle valeur.
+ * Par ailleurs, la variable partagée est par défaut initialisé à 0.
+ *
+ * Pour tester le bon fonctionnement de notre application, nous avons testé plusieurs scénarios:
+ * Le premier est le nombre de sites différents fonctionnant en parallèles. Nous avons tester pour 1 à 3 sites.
+ * Dans tous les cas, nous arrivions bien à récupérer l'accès à la section critique et à modifier la valeur
+ * de la variable partagée. Nous avons de plus vérifier que la variable récupérée par les autres sites après
+ * modification soit bien la nouvelle valeur (elle est donc bien mise à jour dans chaque site).
+ * Nous avons ensuite fait en sorte que la section critique prenne du temps à être traitée. En effet, nous tenions
+ * à vérifier que lorsqu'un site fait une requête alors qu'un autre site se trouve actuellement en section critique,
+ * qu'il attende bien la libération avant de se lancer dans la section critique. Pour prolonger la section critique,
+ * nous avons donc "fait dormir" le thread pendant 10 secondes.
+ *
+ * Nous avons également pu tester que lorsqu'un site demande à modifier la variable, les autres sites peuvent continuer
+ * sans problème à pouvoir lire la variable partagée.
+ *
+ * Nous avons finalement tester l'ordre des accès à la section critique (en utilisant 3 sites distincs). Le site 1 a
+ * fait la demande d'accès et s'est retrouvé en section critique. Pendant qu'il est en section critique, le site 3 émet
+ * une requête. Puis le site 2 fait de même. Les sites 2 et 3 sont donc en attente de la libération de la section
+ * critique par le site 1.
+ * Une fois le site 1 sorti de la section critique, on a pu constater que le site 3 entre en section critique. L'ordre
+ * est donc bien resepcté.
+ *
+ * Etant donnée qu'en RMI chaque message à une réponse, nous avons décidé d'utiliser celle-ci pour, lors d'une requête
+ * de section critique, renvoyer directement la quittance dans la réponse du message. Cela permet d'éviter de générer
+ * des messages supplémentaires et donc de surcharger le réseau.
+ * A noter également que le message de libération (FREE) aura forcément une réponse, bien que celle-ci est inutile
+ * dans le protocole de Lamport.
+ *
+ */
+
 import utils.Constants;
 
 import java.rmi.Naming;
@@ -6,49 +62,7 @@ import java.rmi.registry.LocateRegistry;
 
 
 /**
- * BLABLA
- * - Structure de comment on gère les trucs (genre le common)
- *
- * - Où est la variable partagée
- * Nous avons décidé de placer la variable partagée dans le classe LamportHandler. Celle-ci est un int car nous voulions
- * lors de ce laboratoire faire les choses simplement et ne pas se compliquer la vie. Les taches pourront donc accéder
- * à cet entier et le modifier en proposant un nouvel entier.
- *
- * - Test faits et du sleep
- * Pour tester le bon fonctionnement de notre application, nous avons testé plusieurs scénarios:
- * Le premier est le nombre de sites différents fonctionnant en parallèle .Nous avons tester pour 1 à 3 sites. Dans tous
- * les cas, nous arrivions bien à récupérer la section critique et à modifier la valeur de la variable partagée. Nous avons
- * ensuite réussi à récupérer sa valeur depuis tous les sites. (elle est bien mise à jour dans chaque site.)
- * Nous avons ensuite fait en sorte que la section critique prenne du temps à être traitée. En effet, nous tenions à vérifier
- * que lorsqu'un site fait une requête alors qu'un autre site se trouve actuellement en section critique, qu'il attende
- * bien le FREE avant de se lancer dans la section critique. Pour prolonger la section critique, nous avons donc
- * "fait dormir " le thread pendant 10 secondes.
- *
- * Nous avons également pu tester que lorsqu'un site demande à modifier la variable, les autres sites peuvent continuer
- * sans problème à pouvoir simplement lire la variable commune.
- *
- * Nous avons finalement tester l'ordre des accès à la section critique (en utilisant 3 sites distincs). Le site 1 a
- * fait la demande d'accès et s'est retrouvé en section critique. Pendant qu'il est en section critique, le site 3 emet
- * une requête. Puis le site 2 fait de même. Les sites 2 et 3 sont donc en attente de la libération de la section critique
- * possédée par le site 1.
- * Une fois le site 1 sorti de la section critique, on a pu constater que le site 3 entre en section critique. L'ordre est
- * donc bien resepcté.
- *
- * - Comment on gère le nommage des machins RMI
- *
- * - Retour de quittance dans RMI
- * Compte tenu qu'en RMI chaque message à une réponse, nous avons décidé d'utiliser ceci pour, lors d'une requête, renvoyer
- * directement la quittance. Cela permet d'éviter de générer des messages supplémentaires et donc de surcharger le réseau
- * On peut également constater que le message de libération (FREE) aura forcément une réponse, bien que celle-ci est inutile
- * dans le protocle de Lamport.
- *
- * - Hypothèses: tous les gestionnaires sont lancés avant les clients, on connait tous les noms des machins RMI,
- *
- */
-
-
-/**
- * Classe représentant le gestionnaire de variable partagée. Sa méthode main() va créer l'objet gérant la variable
+ * La méthode main() de la classe Server va créer l'objet gérant la variable
  * globale avant de le mettre a disposition des autres gestionnaires à l'aide d'un registre RMI.
  */
 public class Server {
